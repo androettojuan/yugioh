f0b3c986fbd69196f18d0a98394d68c816827bcd
Juan Manuel Androetto <juan.androetto36@gmail.com>
2022-02-15 19:29:56 +0100

    barra de busqueda

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Cabecera from './components/cabecera/cabecera';
-import Carta from './components/carta/carta';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Carta from "./components/carta/carta";
+
+const BASE_URL = "https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=01/01/2000&enddate=08/23/2002&dateregion=tcg_date"
+
+
+const consultaCarta = async(nombre) => {
+  let url = BASE_URL;
+  if(nombre){
+    url = `${BASE_URL}&fname=${nombre}`
+  }
+
+  const respuesta = await fetch(url);
+  const misDatos = await respuesta.json();
+  return misDatos.data
+}
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [buscarCarta, setBuscarCarta] = useState("");
 
-  useEffect(async ()=>{
-    const respuesta = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=01/01/2000&enddate=08/23/2002&dateregion=tcg_date");
-    const misDatos = await respuesta.json()
-    setData(misDatos.data)
-  },[])
+  useEffect(async () => {
+    const datos = await consultaCarta();
+    setData(datos);
+  }, []);
+
+  const buscar = async () => {
+    const datos = await consultaCarta(buscarCarta);
+    setData(datos);
+  };
 
   return (
-    <div className="yugioh">             
-      {data.map((carta) => <Carta tipo={carta.type} descripcion={carta.desc} name={carta.name} img={carta.card_images[0].image_url_small}/>)}
+    <div className="yugioh">
+      <div>
+        <input value={buscarCarta} onChange={(event) => setBuscarCarta(event.target.value)}></input>
+        <button onClick={buscar}>Buscar</button>
+      </div>
+
+      {data &&
+        data.map((carta) => (
+          <Carta
+            tipo={carta.type}
+            descripcion={carta.desc}
+            name={carta.name}
+            img={carta.card_images[0].image_url_small}
+          />
+        ))}
     </div>
   );
 }
 
 export default App;
+
+//onChange={(event) => setBuscarCarta(event.target.value)}
--- src/components/carta/carta.js
+++ src/components/carta/carta.js
@@ -4,13 +4,13 @@ import "./carta.css";
 const Carta = ({ img, name, descripcion, tipo }) => {
   return (
     <div className="carta">
+      <div className="img">
+        <img src={img} />
+      </div>
       <div className="titulo">
         <h1>{name}</h1>
         <p>Tipo: {tipo}</p>
         <p>Descripcion:{descripcion}</p>
-      </div>
-      <div className="img">
-        <img src={img} />
       </div>
     </div>
   );
